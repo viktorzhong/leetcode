fc0c86b1a2e5f729fd1e30916508cadc3fc7be14
viktorzhong <vzhong81@gmail.com>
2020-01-18 00:47:20 +0800

    Update solutions

---
+++ Go/max_depth.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func max(x, y int) int {
+    if x > y {
+        return x
+    }
+
+    return y
+}
+
+func maxDepth(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+
+    return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
