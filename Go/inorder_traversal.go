1848358cc98eb95a7a68df24523cd18c55f512ae
viktorzhong <vzhong81@gmail.com>
2020-01-17 10:27:18 +0800

    Update solutions.

---
+++ Go/inorder_traversal.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+    if root == nil {
+      return []int{}
+    }
+    
+    left := inorderTraversal(root.Left)
+    right := inorderTraversal(root.Right)
+    left = append(left, root.Val)
+    left = append(left, right...)
+
+    return left
+}
