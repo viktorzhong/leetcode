fda34d8a5b2bf09bf5fd5e93aa339e4d21aaaed6
viktorzhong <vzhong81@gmail.com>
2020-01-17 17:53:14 +0800

    Update solution

--- Java/SymmetricTree.java
+++ Java/SymmetricTree.java
@@ -8,6 +8,7 @@
  * }
  */
 class Solution {
+    // Recursive solution
     public boolean isSymmetric(TreeNode root) {
         if(root == null) return true;
 
@@ -21,5 +22,24 @@ class Solution {
         }
 
         return l == null && r == null;
+    }
+
+    // Iterative solution  非递归解法
+    public boolean isSymmetricIterative(TreeNode root) {
+        if(root == null) return true;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root.left);
+        stack.push(root.right);
+
+        while(!stack.isEmpty()) {
+            TreeNode r = stack.pop(), l = stack.pop();
+            if(r == null && l == null) continue;
+            if(r == null || l == null) return false;
+            if(r.val != l.val) return false;
+
+            stack.push(l.left); stack.push(r.right);
+            stack.push(l.right); stack.push(r.left);
+        }
+        return true;
     }
 }
