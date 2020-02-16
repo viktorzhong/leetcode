577bdbf6b3be4a1f0cc73db5e37c3583243e8606
viktorzhong <vzhong81@gmail.com>
2020-02-16 14:23:47 +0800

    Update solutions.

--- Java/HouseRobber.java
+++ Java/HouseRobber.java
@@ -18,4 +18,21 @@ class Solution {
         }
         return solve(0, nums);    
     }
+	
+	public int rob2(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        int[] d = new int[n];
+        d[0] = nums[0];
+        d[1] = Math.max(nums[0], nums[1]);
+        int res = Math.max(d[0], d[1]);
+
+        for(int i = 2; i < n; i++) {
+            d[i] = Math.max(d[i - 2] + nums[i], d[i - 1]);
+            res = Math.max(res, d[i]);
+        }
+
+        return res;
+    }
 }
