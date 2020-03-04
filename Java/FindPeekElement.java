188414abc802fc95e8d92c4f25869594fff9cd12
viktorzhong <vzhong81@gmail.com>
2020-03-04 21:02:33 +0800

    Update solutions.

---
+++ Java/FindPeekElement.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        return search(nums, 0, nums.length - 1);
+    }
+
+    private int search(int[] nums, int l, int r) {
+        if(l == r) return l;
+        int mid = l + (r - l) / 2;
+        if(nums[mid] > nums[mid + 1])
+            return search(nums, l, mid);
+        
+        return search(nums, mid + 1, r);
+    }
+}
